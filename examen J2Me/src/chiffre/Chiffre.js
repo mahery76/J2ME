a504040e06dd708c3b281fc27eed1bca39ad0da4
mahery76 <randrianjafymahery@gmail.com>
2023-01-17 12:05:28 +0300

    done using chat gpt algo

--- examen J2Me/src/chiffre/Chiffre.js
+++ examen J2Me/src/chiffre/Chiffre.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { arabicToRoman, romanToArabic } from './convert'
 
 function Chiffre() {
+  const inputRef = useRef(null)
+  const [inputType, setInputType] = useState("number")
+
+
+  const [secondUnit, setSecondUnit] = useState("chiffre romain")
+  const [secondValue, setSecondValue] = useState("")
+
+  function handleChange(event) {
+    if (event.target.value == "CA") {
+      setSecondUnit("chiffre romain")
+      setInputType("number")
+    }
+    else {
+      setSecondUnit("chiffre arabe")
+      setInputType("text")
+
+    }
+  }
+  function handleClick(event) {
+    event.preventDefault()
+    console.log('mande ny boutton ')
+    if (secondUnit == "chiffre romain") {
+      setSecondValue(arabicToRoman(inputRef.current.value))
+    }
+    else {
+      setSecondValue(romanToArabic(inputRef.current.value))
+    }
+  }
   return (
-    <div>C</div>
+    <div className='fen chiffre'>
+      <br />
+      <br />
+      <br />
+      <div className="titre"></div>
+      <div className="valeurs">
+
+        {/* premier valeur  */}
+        <div className="values"><input type={inputType} ref={inputRef} /> </div>
+        <select name="" id="firstUnit" onChange={handleChange} defaultValue="CA" >
+          <option value="CA">chiffre arabe</option>
+          <option value="CR">chiffre romain</option>
+        </select>
+
+        {/* signe egale  */}
+        <div className="egale"> = </div>
+
+        {/* deuxieme valeur  */}
+        <div className="valeur">
+          <div className="secondValue">{secondValue}</div>
+          <div className="secondUnit">
+            {secondUnit}
+          </div>
+        </div>
+
+      </div>
+
+      <div className="boutton">
+        <button onClick={handleClick}>Convertir</button>
+      </div>
+
+    </div>
   )
 }
 
--- examen J2Me/src/temperature/Temperature.js
+++ examen J2Me/src/temperature/Temperature.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 function Temperature() {
+    const inputRef = useRef(null)
+
+    const [secondUnit, setSecondUnit] = useState("Degré Fahrenheit")
+    const [secondValue, setSecondValue] = useState(0)
+
+    function handleChange(event) {
+        if (event.target.value == "DC") {
+            setSecondUnit("Degré Fahrenheit")
+        }
+        else {
+            setSecondUnit("Degré Celsius")
+        }
+    }
+
+    function handleClick(event){
+        if (secondUnit == "Degré Fahrenheit") {
+            setSecondValue((inputRef.current.value * 9 / 5) + 32)
+        }
+        else {
+            setSecondValue((inputRef.current.value - 32) * 5 / 9)
+        }
+    }
+
     return (
         <div className="fen temperature">
             <div className="titre">
             </div>
-            
+
             <div className="valeurs">
                 {/* premier valeur  */}
                 <div className="valeur">
-                    <div className="values"> <input type="number" /> </div>
-                    <div className="unit" />
+                    <div className="values"> <input type="number" ref={inputRef} /> </div>
+                    <select id="firstUnit" onChange={handleChange} defaultValue="DC" >
+                        <option value="DC">Degré Celsius</option>
+                        <option value="DF">Degré Fahrenheit</option>
+                    </select>
                 </div>
 
                 {/* signe egale  */}
@@ -18,13 +44,16 @@ function Temperature() {
 
                 {/* deuxieme valeur  */}
                 <div className="valeur">
-                    <div className="values" />
-                    <div className="unit" />
+                    <div className="secondValue">{secondValue}</div>
+                    <div className="secondUnit">
+                        {secondUnit}
+                    </div>
+
                 </div>
 
             </div>
-            <div className="button">
-                <button>Convertir</button>
+            <div className="boutton"> 
+                <button onClick = {handleClick}>Convertir</button>
             </div>
         </div>
     )
